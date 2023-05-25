391a894a4d92175b643aac7780ddcf9b4d258514
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-05-25 08:45:07 +0200

    mas huffman

--- Ejercicio 7/Ejercicio8.py
+++ Ejercicio 7/Ejercicio8.py
@@ -23,3 +23,50 @@ def crear_arbol(frecuencias):
         nodos.remove(derecho)
         nodos.append(nuevo_nodo)
     return nodos[0]
+
+def generar_codigos(nodo, valor='', camino=''):
+    nuevo_val = valor + str(nodo.huff)
+    if nodo.izquierdo:
+        camino = generar_codigos(nodo.izquierdo, nuevo_val, camino)
+    if nodo.derecho:
+        camino = generar_codigos(nodo.derecho, nuevo_val, camino)
+
+    if not nodo.izquierdo and not nodo.derecho:
+        camino += ('%s%s%s\n' % (nodo.simbolo, ' : ', nuevo_val))
+    return camino
+
+frecuencias = {
+    'T': 0.15,
+    'O': 0.15,
+    'A': 0.12,
+    'E': 0.10,
+    'H': 0.09,
+    'S': 0.07,
+    'P': 0.07,
+    'M': 0.07,
+    'N': 0.06,
+    'C': 0.06,
+    'D': 0.05,
+    'Z': 0.04,
+    'K': 0.03,
+    ',': 0.03
+}
+
+arbol = crear_arbol(frecuencias)
+codigos = generar_codigos(arbol)
+codigos = {line.split(' : ')[0]: line.split(' : ')[1][:-1] for line in codigos.split('\n') if line}
+
+def comprimir(mensaje):
+    return ''.join(codigos[i] for i in mensaje)
+
+def descomprimir(mensaje):
+    clave = ''
+    res = ''
+    for i in mensaje:
+        clave += i
+        for k, v in codigos.items():
+            if clave == v:
+                res += k
+                clave = ''
+                break
+    return res
