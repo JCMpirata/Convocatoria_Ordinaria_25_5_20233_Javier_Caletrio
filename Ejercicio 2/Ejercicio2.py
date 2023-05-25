320306ad98217763d72b8778c3c826e30219aa05
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-05-25 10:55:07 +0200

    ejercicio 2 terminado

--- Ejercicio 2/Ejercicio2.py
+++ Ejercicio 2/Ejercicio2.py
@@ -18,16 +18,38 @@ class Pokemon:
         else:
             return "Desconocido"
 
-# Crear una lista con Pokemon
-pokemones = [
-    Pokemon("Squirtle", "Agua"),
-    Pokemon("Charizard", "Fuego"),
-    Pokemon("Bulbasaur", "Planta"),
-    Pokemon("Pikachu", "Eléctrico"),
-    Pokemon("Articuno", "Hielo"),
-    Pokemon("Pidgeot", "Volador")
-]
+class Pokemon:
+    def __init__(self, nombre, tipo):
+        self.nombre = nombre
+        self.tipo = tipo
+        print(f"El Pokemon {self.nombre} se ha creado con éxito.")
 
-# Clasificar los Pokemon
-for pokemon in pokemones:
-    print(f"{pokemon.nombre}: {pokemon.clasificacion()}")
+    def clasificacion(self):
+        clasificacion_tipo = {
+            "Agua": "PS",
+            "Fuego": "Ataque",
+            "Planta": "Defensa",
+            "Eléctrico": "Ataque Especial",
+            "Hielo": "Defensa Especial",
+            "Volador": "Velocidad"
+        }
+        if self.tipo in clasificacion_tipo:
+            return clasificacion_tipo[self.tipo]
+        else:
+            return "Desconocido"
+if __name__ == '__main__':
+
+    # Crear una lista con Pokemon
+    pokemones = [
+        Pokemon("Squirtle", "Agua"),
+        Pokemon("Charizard", "Fuego"),
+        Pokemon("Bulbasaur", "Planta"),
+        Pokemon("Pikachu", "Eléctrico"),
+        Pokemon("Articuno", "Hielo"),
+        Pokemon("Pidgeot", "Volador")
+    ]
+
+    # Clasificar los Pokemon
+    for pokemon in pokemones:
+        print(f"{pokemon.nombre}: {pokemon.clasificacion()}")
+
