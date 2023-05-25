80d354820b7d9f5aa7c9cb23815c17ddf6ed7337
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-05-25 10:38:01 +0200

    corregir ejercicio 5

--- Ejercicio 5/Ejercicio5.py
+++ Ejercicio 5/Ejercicio5.py
@@ -1,54 +1,9 @@
-class MovimientoAbra:
-    def __init__(self):
-        self.matrix = [
-            [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
-            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 1, 1],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-            [0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
-            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0, 1, 0, 0, 0, 0]
-        ]
+class Nodo:
+    def __init__(self, ruta):
+        self.ruta = ruta
+        self.vecinos = []
 
-    def movimiento_caballo(self, n):
-        posibilidades_validas = self.matrix_power(n)
-        print("Posibilidades validas: ", posibilidades_validas)
-
-        c = self.sumar_posibilidades_validas(posibilidades_validas)
-        print("Cantidad total de posibilidades validas: ", c)
-
-    def matrix_power(self, n):
-        if n == 0:
-            return [[1 if i == j else 0 for j in range(10)] for i in range(10)]
-        elif n == 1:
-            return self.matrix.copy()
-        else:
-            half_power = self.matrix_power(n // 2)
-            result = self.multiply_matrices(half_power, half_power)
-            if n % 2 == 1:
-                result = self.multiply_matrices(result, self.matrix)
-            return result
-
-    def multiply_matrices(self, matrix1, matrix2):
-        result = [[0] * 10 for _ in range(10)]
-        for i in range(10):
-            for j in range(10):
-                for k in range(10):
-                    result[i][j] += matrix1[i][k] * matrix2[k][j]
-        return result
-
-    def sumar_posibilidades_validas(self, matrix):
-        c = 0
-        for i in range(10):
-            for j in range(10):
-                c += matrix[i][j]
-        return c
+    def agregar_vecino(self, vecino):
+        self.vecinos.append(vecino)
 
 
-if __name__ == "__main__":
-    movimientos_abra = MovimientoAbra()
-    n = int(input("Ingrese un numero: "))
-    movimientos_abra.movimiento_caballo(n)
