13bab7f48a34d2dfb8ae1e7f99af8d2aaf3b98ac
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-05-25 10:39:22 +0200

    5 terminado

--- Ejercicio 5/Ejercicio5.py
+++ Ejercicio 5/Ejercicio5.py
@@ -47,4 +47,35 @@ class MovimientoAbra:
         self.nodos[9].agregar_vecino(self.nodos[2])
         self.nodos[9].agregar_vecino(self.nodos[4])
 
-    
+    def movimiento_caballo(self, n):
+        posibilidades_validas = self.calcular_posibilidades_validas(n)
+        print("Posibilidades válidas: ", posibilidades_validas)
+
+        c = sum(posibilidades_validas.values())
+        print("Cantidad total de posibilidades válidas: ", c)
+
+    def calcular_posibilidades_validas(self, n):
+        if n in self.cantidad_posibilidades:
+            return self.cantidad_posibilidades[n]
+
+        if n == 1:
+            posibilidades = {nodo.ruta: 1 for nodo in self.nodos}
+        else:
+            posibilidades = {nodo.ruta: 0 for nodo in self.nodos}
+            for nodo in self.nodos:
+                for vecino in nodo.vecinos:
+                    sub_posibilidades = self.calcular_posibilidades_validas(n - 1)
+                    for ruta, cantidad in sub_posibilidades.items():
+                        if ruta == nodo.ruta:
+                            posibilidades[nodo.ruta] += cantidad
+                        else:
+                            posibilidades[vecino.ruta] += cantidad
+
+        self.cantidad_posibilidades[n] = posibilidades
+        return posibilidades
+
+
+if __name__ == "__main__":
+    movimientos_abra = MovimientoAbra()
+    n = int(input("Ingrese un número: "))
+    movimientos_abra.movimiento_caballo(n)
