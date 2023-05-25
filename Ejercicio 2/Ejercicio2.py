f490961f3b35227ced4dce6bc26b0f8464798e58
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-05-25 10:56:32 +0200

    text ejercicio 2

--- Ejercicio 2/Ejercicio2.py
+++ Ejercicio 2/Ejercicio2.py
@@ -1,22 +1,4 @@
-class Pokemon:
-    def __init__(self, nombre, tipo):
-        self.nombre = nombre
-        self.tipo = tipo
-        print(f"El Pokemon {self.nombre} se ha creado con éxito.")
-
-    def clasificacion(self):
-        clasificacion_tipo = {
-            "Agua": "PS",
-            "Fuego": "Ataque",
-            "Planta": "Defensa",
-            "Eléctrico": "Ataque Especial",
-            "Hielo": "Defensa Especial",
-            "Volador": "Velocidad"
-        }
-        if self.tipo in clasificacion_tipo:
-            return clasificacion_tipo[self.tipo]
-        else:
-            return "Desconocido"
+import unittest
 
 class Pokemon:
     def __init__(self, nombre, tipo):
@@ -37,6 +19,37 @@ class Pokemon:
             return clasificacion_tipo[self.tipo]
         else:
             return "Desconocido"
+
+class Pokemon:
+    def __init__(self, nombre, tipo):
+        self.nombre = nombre
+        self.tipo = tipo
+        print(f"El Pokemon {self.nombre} se ha creado con éxito.")
+
+    def clasificacion(self):
+        clasificacion_tipo = {
+            "Agua": "PS",
+            "Fuego": "Ataque",
+            "Planta": "Defensa",
+            "Eléctrico": "Ataque Especial",
+            "Hielo": "Defensa Especial",
+            "Volador": "Velocidad"
+        }
+        if self.tipo in clasificacion_tipo:
+            return clasificacion_tipo[self.tipo]
+        else:
+            return "Desconocido"
+        
+# Hacer pruebas de la clase Pokemon
+class TestPokemon(unittest.TestCase):
+        def test_pokemon(self):
+            pokemon = Pokemon("Squirtle", "Agua")
+            self.assertEqual(pokemon.nombre, "Squirtle")
+            self.assertEqual(pokemon.tipo, "Agua")
+            self.assertEqual(pokemon.clasificacion(), "PS")
+            self.assertEqual(pokemon.__str__(), "Nombre: Squirtle, Tipo: Agua")
+
+
 if __name__ == '__main__':
 
     # Crear una lista con Pokemon
@@ -52,4 +65,6 @@ if __name__ == '__main__':
     # Clasificar los Pokemon
     for pokemon in pokemones:
         print(f"{pokemon.nombre}: {pokemon.clasificacion()}")
+
+    unittest.main()
 
